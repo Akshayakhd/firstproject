ebec8415ae07500bb9aec3d7dc8814c3fae8f224
Akshayakhd <akshayakhd2023@gmail.com>
2026-06-09 15:20:44 +0530

    chnge

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:firstproject/container.dart';
 import 'package:firstproject/profile.dart';
 import 'package:firstproject/sample.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: profile()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: containerx()));
 }
